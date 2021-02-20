469db2299322b8db7c2e8f1fb45c4e1b827bc08a
thejj26 <65808980+thejj26@users.noreply.github.com>
2021-02-20 14:18:12 +0100

    Add files via upload
    Prvi commitani file. Napravljen glavni izgled stranice i opis za sve vrste komponenata.

---
+++ zadnjiProjekt/code/main.js
@@ -0,0 +1,6 @@
+M.AutoInit()
+
+window.addEventListener("load", () => {
+    document.getElementById("topText").style.opacity = "1"
+    document.getElementById("topText").style.marginTop = "7vh"
+})
