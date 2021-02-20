41dc920dd7f7a5443a1c2363870bad97c772ec6c
thejj26 <jakov.jozic26@gmail.com>
2021-02-20 18:28:44 +0100

    Dodan logo i Materialize carousel

--- zadnjiProjekt/code/main.js
+++ zadnjiProjekt/code/main.js
@@ -1,6 +1,31 @@
+/*Materialize carousel*/
 M.AutoInit()
+
+$(document).ready(function () {
+    $('.carousel').carousel({
+        numVisible: 1
+    });
+});
 
 window.addEventListener("load", () => {
     document.getElementById("topText").style.opacity = "1"
     document.getElementById("topText").style.marginTop = "7vh"
 })
+
+const prev = document.getElementById("prev")
+const next = document.getElementById("next")
+prev.addEventListener("click", () => {
+    $('.carousel').carousel('prev')
+})
+next.addEventListener("click", () => {
+    $('.carousel').carousel('next')
+})
+
+/*Prevencija selektiranja kod brzog klikavanja*/
+
+prev.addEventListener('mousedown', function (e) {
+    e.preventDefault();
+}, false);
+next.addEventListener('mousedown', function (e) {
+    e.preventDefault();
+}, false);
